e9b2e53981af007213917c1e057b1ba128a4f6fe
scart97 <scart.lucas@gmail.com>
2021-08-17 11:32:19 -0300

    refactor(quartznet): clean exporting code

--- create_quartznet.py
+++ create_quartznet.py
@@ -1,25 +1,8 @@
 from thunder.quartznet.module import QuartznetModule, QuartznetCheckpoint
 from thunder.quartznet.transform import patch_stft
-from thunder.data.dataset import AudioFileLoader
 
 model = QuartznetModule.load_from_nemo(QuartznetCheckpoint.QuartzNet5x5LS_En)
 model.eval()
 model.audio_transform = patch_stft(model.audio_transform)
-print(model.audio_transform[2].stft_func)
-loader = AudioFileLoader()
-#audio = loader("scent_of_a_woman_future.wav")
-
-#transcription = model.predict(audio)
-#print(transcription)
 
 model.to_torchscript("app/src/main/assets/model.pt")
-# traced_model = torch.jit.trace(model, input_values, strict=False)
-# model_dynamic_quantized = torch.quantization.quantize_dynamic(
-#     model, qconfig_spec={torch.nn.Linear}, dtype=torch.qint8
-# )
-# traced_quantized_model = torch.jit.trace(
-#     model_dynamic_quantized, input_values, strict=False
-# )
-
-# optimized_traced_quantized_model = optimize_for_mobile(traced_quantized_model)
-# optimized_traced_quantized_model.save("app/src/main/assets/wav2vec_traced_quantized.pt")
